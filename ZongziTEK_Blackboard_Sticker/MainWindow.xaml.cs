19032052425aa2c2eafebd5623fef93b7d3e4d9c
STBBRD <62409850+STBBRD@users.noreply.github.com>
2022-05-30 21:50:02 +0800

    添加项目文件。

---
+++ ZongziTEK_Blackboard_Sticker/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Ink;
+using System.IO;
+using System.Drawing;
+
+namespace ZongziTEK_Blackboard_Sticker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DrawingAttributes drawingAttributes;
+        public MainWindow()
+        {
+            InitializeComponent();
+            drawingAttributes = new DrawingAttributes();
+            inkCanvas.DefaultDrawingAttributes = drawingAttributes;
+            drawingAttributes.Color = Colors.White;
+            drawingAttributes.Width = 3;
+            drawingAttributes.Height = 2;
+            drawingAttributes.StylusTip = StylusTip.Ellipse;
+            drawingAttributes.FitToCurve = true;
+            drawingAttributes.IgnorePressure = true;
+        }
+
+        private void penButton_Click(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+        }
+
+        private void eraserButton_Click(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.EditingMode = InkCanvasEditingMode.EraseByStroke;
+        }
+
+        private void window_StateChanged(object sender, EventArgs e)
+        {
+            if (WindowState == WindowState.Minimized)
+            {
+                WindowState = WindowState.Normal;
+            }
+        }
+
+        private void window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Top = 20;
+            Left = SystemParameters.WorkArea.Width - 395;
+        }
+
+        private void scrShot_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
