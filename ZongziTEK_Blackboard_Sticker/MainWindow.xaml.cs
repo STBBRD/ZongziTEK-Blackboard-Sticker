45a1b8bd018ecd8b99c343798e9cc912e28ad92d
STBBRD <62409850+STBBRD@users.noreply.github.com>
2022-11-12 22:24:52 +0800

    更改UI

--- ZongziTEK_Blackboard_Sticker/MainWindow.xaml.cs
+++ ZongziTEK_Blackboard_Sticker/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Ink;
-using System.IO;
-using System.Drawing;
+using System.Windows.Media;
 
 namespace ZongziTEK_Blackboard_Sticker
 {
@@ -58,12 +46,13 @@ namespace ZongziTEK_Blackboard_Sticker
         private void window_Loaded(object sender, RoutedEventArgs e)
         {
             Top = 20;
-            Left = SystemParameters.WorkArea.Width - 395;
+            Left = SystemParameters.WorkArea.Width - 460;
+            Height = System.Windows.SystemParameters.PrimaryScreenHeight - 440;
         }
 
         private void scrShot_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
     }
 }
