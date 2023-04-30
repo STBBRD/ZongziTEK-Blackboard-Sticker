006b2405a653b831576df6010392b81aea31f82d
STBBRD <62409850+STBBRD@users.noreply.github.com>
2023-04-30 10:26:16 +0800

    防止多开

--- ZongziTEK_Blackboard_Sticker/App.xaml.cs
+++ ZongziTEK_Blackboard_Sticker/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Reflection;
+using System;
+using System.Windows;
 
 namespace ZongziTEK_Blackboard_Sticker
 {
@@ -7,5 +10,23 @@ namespace ZongziTEK_Blackboard_Sticker
     /// </summary>
     public partial class App : Application
     {
+        System.Threading.Mutex mutex;
+
+        public App()
+        {
+            this.Startup += new StartupEventHandler(App_Startup);
+        }
+
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+            bool ret;
+            mutex = new System.Threading.Mutex(true, "ZongziTEK_Blackboard_Sticker", out ret);
+
+            if (!ret)
+            {
+                MessageBox.Show("已有一个黑板贴正在运行");
+                Environment.Exit(0);
+            }
+        }
     }
 }
