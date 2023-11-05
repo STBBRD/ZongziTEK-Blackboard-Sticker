df1b0a0cd9f79428b893badb3a34c2125d14fcc8
STBBRD <62409850+STBBRD@users.noreply.github.com>
2023-11-05 08:40:10 +0800

    Revert "Merge branch 'master' of https://github.com/STBBRD/ZongziTEK-Blackboard-Sticker"
    This reverts commit bb45d5c590827cf45a59ce4093162c68f9520e4d, reversing
    changes made to 90f8b1604094b41a6d4c7ca7be0fe3284975f9e4.

--- ZongziTEK_Blackboard_Sticker/Classes/Settings.cs
+++ ZongziTEK_Blackboard_Sticker/Classes/Settings.cs
@@ -31,8 +31,5 @@ namespace ZongziTEK_Blackboard_Sticker
     public class TimetableSettings
     {
         public bool useTimetable { get; set; } = true;
-        public bool enableTimetableNotification { get; set; } = true;
-        public bool useDefaultBNSPath = true;
-        public string BNSPath { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
     }
 }
