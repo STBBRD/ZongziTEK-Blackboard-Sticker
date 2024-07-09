2e7fb6c5eeed14e575dacf6b059179e2e4332af6
STBBRD <62409850+STBBRD@users.noreply.github.com>
2024-07-09 11:57:59 +0800

    将注册表文件下载链接替换为直链

--- ZongziTEK_Blackboard_Sticker/Pages/SettingsPages/AutomationSettingsPage.xaml.cs
+++ ZongziTEK_Blackboard_Sticker/Pages/SettingsPages/AutomationSettingsPage.xaml.cs
@@ -28,7 +28,7 @@ namespace ZongziTEK_Blackboard_Sticker.Pages.SettingsPages
 
         private void HyperlinkButton_PreviewMouseUp(object sender, MouseButtonEventArgs e)
         {
-            Process.Start("https://alist.xrgzs.top/%E6%99%BA%E8%83%BD%E7%99%BD%E6%9D%BF%E9%95%9C%E5%83%8F/seewo/!Files/%E5%B8%8C%E6%B2%83%E6%9C%8D%E5%8A%A1%E5%8F%8A%E5%B8%8C%E6%B2%83%E7%AE%A1%E5%AE%B6/%E5%B8%8C%E6%B2%83%E7%AE%A1%E5%AE%B6%E7%BA%AF%E5%87%80%E6%A8%A1%E5%BC%8F.reg");
+            Process.Start("https://alist.xrgzs.top/d/pxy/eduimg/seewo/!Files/%E5%B8%8C%E6%B2%83%E6%9C%8D%E5%8A%A1%E5%8F%8A%E5%B8%8C%E6%B2%83%E7%AE%A1%E5%AE%B6/%E5%B8%8C%E6%B2%83%E7%AE%A1%E5%AE%B6%E7%BA%AF%E5%87%80%E6%A8%A1%E5%BC%8F.reg");
         }
     }
 }
