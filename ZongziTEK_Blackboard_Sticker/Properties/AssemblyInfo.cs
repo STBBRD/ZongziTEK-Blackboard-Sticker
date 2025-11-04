0005e4784db2d210629b3f8f9cee967cb6bab573
ZongziTEK <62409850+STBBRD@users.noreply.github.com>
2025-11-04 12:37:57 +0800

    Update AssemblyInfo.cs

--- ZongziTEK_Blackboard_Sticker/Properties/AssemblyInfo.cs
+++ ZongziTEK_Blackboard_Sticker/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ZongziTEK")]
 [assembly: AssemblyProduct("ZongziTEK Blackboard Sticker")]
-[assembly: AssemblyCopyright("Copyright © ZongziTEK 2024")]
+[assembly: AssemblyCopyright("ZongziTEK")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -49,5 +49,5 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.0.0")]
-[assembly: AssemblyFileVersion("3.3.0.0")]
+[assembly: AssemblyVersion("3.3.0.1")]
+[assembly: AssemblyFileVersion("3.3.0.1")]
